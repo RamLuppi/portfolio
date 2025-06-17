4d0dcf2c99c0c29af88a3e0cf70b2ac4c285db0b
Ramiro Luppi <ramiro.luppi@gmail.com>
2025-06-17 00:14:21 -0300

    Create EmailNegocio.cs
    sin datos privados

---
+++ source/repos/AppWeb-public/negocio/EmailNegocio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+namespace negocio
+{
+    public class EmailNegocio
+    {
+        private SmtpClient server;
+        private MailMessage email;
+
+        public EmailNegocio()
+        {
+
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("X", "XX");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+        }
+
+        public void armarMail(string EmailContacto, string nombre, string mensaje)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("X");
+            email.To.Add("Mi mail");
+            string cuerpo = "Nuevo envio de correo electronico por cuestionario. El mail del contacto es " + EmailContacto + " . El nombre: " + nombre + " . Y el mensaje que ha dejado: " + mensaje + " .";
+            email.Body = cuerpo;
+        }
+
+        public void enviarMail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
